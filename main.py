4756beffc5eaecc7ff4bbe1222a15477927cc186
tapochek97 <tapochek97@ya.ru>
2017-01-22 16:34:38 +0300

    0.2

--- main.py
+++ main.py
@@ -10,16 +10,16 @@ csv_test_file = r"test_file.csv"
 text_test_output_file = r"news_output.txt"
 
 print('Getting training file...')
-get_training_csv(txt_train_input_file, csv_train_input_file)
+# get_training_csv(txt_train_input_file, csv_train_input_file)
 print('')
 
 print('Cleaning training text...')
-train = clean_text(csv_train_input_file)
+# train = clean_text(csv_train_input_file)
 print('')
 
 print('Tokenization training file...')
 start = time.clock()
-tokenization(train, csv_train_file)
+# tokenization(train, csv_train_file)
 stop = time.clock()
 print('Time of tokenization: ', stop - start, ' s')
 train = read_csv(csv_train_file, encoding = 'utf-8')
@@ -28,18 +28,16 @@ print('')
 
 print('Vectorizing training text...')
 from sklearn.feature_extraction.text import CountVectorizer
-# from sklearn.feature_extraction.text import TfidfTransformer
-coder = CountVectorizer()
+from sklearn.feature_extraction.text import HashingVectorizer
+coder = HashingVectorizer()
 start = time.clock()
 trn = coder.fit_transform(train.tokens)
 stop = time.clock()
 print('Time of vectorization: ', stop - start, ' s')
-# tfidf_transformer = TfidfTransformer()
-# train_tfidf = tfidf_transformer.fit_transform(trn)
 print('')
 
 print('Creating model...')
-# from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import MultinomialNB
 from sklearn.svm import LinearSVC
 start = time.clock()
 clf = LinearSVC().fit(trn, train.rubric)
@@ -48,16 +46,16 @@ print('Time of model creation: ', stop - start, ' s')
 print('')
 
 print('Getting test file...')
-get_test_csv(txt_test_input_file, csv_test_input_file)
+# get_test_csv(txt_test_input_file, csv_test_input_file)
 print('')
 
 print('Cleaning testing text...')
-test = clean_text(csv_test_input_file)
+# test = clean_text(csv_test_input_file)
 print('')
 
 print('Tokenization testing file...')
 start = time.clock()
-tokenization_test(test, csv_test_file)
+# tokenization_test(test, csv_test_file)
 stop = time.clock()
 print('Time of tokenization: ', stop - start, ' s')
 test = read_csv(csv_test_file, encoding = 'utf-8')
@@ -69,7 +67,6 @@ start = time.clock()
 tst = coder.transform(test.tokens)
 stop = time.clock()
 print('Time of vectorization: ', stop - start, ' s')
-# test_tfidf = tfidf_transformer.ransform(tst)
 print('')
 
 print('Predicting rubric for each test news...')
@@ -77,5 +74,5 @@ result = clf.predict(tst)
 print('')
 
 print('Outputting result...')
-get_output(test, result, text_test_output_file)
+get_output(result, text_test_output_file)
 print("That's all!")
--- text_edit.py
+++ text_edit.py
@@ -26,7 +26,7 @@ def clean_text(csv_input_file):
 
     # Cleaning text from useless characters
     train.title = train.title.str.replace(' - ', ' ')
-    train.title = train.title.str.replace('[0-9]', '')
+    train.title = train.title.str.replace('[0-9]', ' ')
     train.title = train.title.str.replace('- ', ' ')
     train.title = train.title.str.replace(' -', ' ')
     train.title = train.title.str.replace(u' . ', ' ')
@@ -42,8 +42,38 @@ def clean_text(csv_input_file):
     train.title = train.title.str.replace('»', ' ')
     train.title = train.title.str.replace(u' +', ' ')
     train.title = train.title.str.strip()
+    train.text = train.text.str.replace('А', ' А')
+    train.text = train.text.str.replace('Б', ' Б')
+    train.text = train.text.str.replace('В', ' В')
+    train.text = train.text.str.replace('Г', ' Г')
+    train.text = train.text.str.replace('Д', ' Д')
+    train.text = train.text.str.replace('Е', ' Е')
+    train.text = train.text.str.replace('Ё', ' Ё')
+    train.text = train.text.str.replace('Ж', ' Ж')
+    train.text = train.text.str.replace('З', ' З')
+    train.text = train.text.str.replace('И', ' И')
+    train.text = train.text.str.replace('Й', ' Й')
+    train.text = train.text.str.replace('К', ' К')
+    train.text = train.text.str.replace('Л', ' Л')
+    train.text = train.text.str.replace('М', ' М')
+    train.text = train.text.str.replace('Н', ' Н')
+    train.text = train.text.str.replace('О', ' О')
+    train.text = train.text.str.replace('П', ' П')
+    train.text = train.text.str.replace('Р', ' Р')
+    train.text = train.text.str.replace('С', ' С')
+    train.text = train.text.str.replace('Т', ' Т')
+    train.text = train.text.str.replace('У', ' У')
+    train.text = train.text.str.replace('Ф', ' Ф')
+    train.text = train.text.str.replace('Х', ' Х')
+    train.text = train.text.str.replace('Ц', ' Ц')
+    train.text = train.text.str.replace('Ч', ' Ч')
+    train.text = train.text.str.replace('Ш', ' Ш')
+    train.text = train.text.str.replace('Щ', ' Щ')
+    train.text = train.text.str.replace('Э', ' Э')
+    train.text = train.text.str.replace('Ю', ' Ю')
+    train.text = train.text.str.replace('Я', ' Я')
     train.text = train.text.str.replace(' - ', ' ')
-    train.text = train.text.str.replace('[0-9]', '')
+    train.text = train.text.str.replace('[0-9]', ' ')
     train.text = train.text.str.replace('- ', ' ')
     train.text = train.text.str.replace(' -', ' ')
     train.text = train.text.str.replace(u' . ', ' ')
@@ -107,7 +137,7 @@ def tokenization_test(test, csv_test_file):
             out_csv.writerow([[], my_tokenizer(y, morph)])
 
 
-def get_output(test, result, output_file):
+def get_output(result, output_file):
     with open(output_file, mode = 'w', encoding = 'utf-8') as output:
         output.write('\n'.join(result))
 
